5eaa21c47f6b08be7c9e1980752312efd80d0ee9
Franccesco Jaimes <fjaimes@pucp.edu.pe>
2023-08-23 01:26:56 -0500

    feat(ms-auth): add database config, filters and interceptors

--- backend/ms-auth/src/auth.module.ts
+++ backend/ms-auth/src/auth.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './common/database';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter, LoggingInterceptor, TimeOutInterceptor } from './common';
+import * as Joi from 'joi';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeOutInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AuthModule {}
