c6ff3adb5d069e5f1aa83df63ae3f1981a683bef
Franccesco Jaimes <fjaimes@pucp.edu.pe>
2023-08-23 01:28:49 -0500

    feat(ms-notifications): add database config, filters and interceptors

--- backend/ms-notifications/src/main.ts
+++ backend/ms-notifications/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationsModule } from './notifications.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT');  
+  await app.listen(port).then(() => {
+    console.log(`Notifications service is running on port ${port}`);
+  });
 }
 bootstrap();
--- backend/ms-notifications/src/notifications.module.ts
+++ backend/ms-notifications/src/notifications.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AllExceptionFilter, LoggingInterceptor, TimeOutInterceptor } from './common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './common/database';
+import * as Joi from 'joi';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [
+    NotificationsService,
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeOutInterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class NotificationsModule {}
