283f04a107134728e5e7e2bd1b8a75b82068f540
Franccesco Jaimes <fjaimes@pucp.edu.pe>
2023-08-24 18:26:38 -0500

    fix(ms-queries): add cors and cookie-parser

--- backend/ms-queries/src/main.ts
+++ backend/ms-queries/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
-  const port = configService.get<string>('PORT');  
+  const port = configService.get<string>('PORT');
+  app.use(cookieParser());
   await app.listen(port).then(() => {
     console.log(`Queries service is running on port ${port}`);
   });
