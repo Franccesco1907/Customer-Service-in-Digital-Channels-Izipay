39e488fa164d41a703f84238138a69fe59e06e09
Franccesco Jaimes <fjaimes@pucp.edu.pe>
2023-08-24 21:57:25 -0500

    feat(ms-notifications): add notifications service

--- backend/ms-queries/src/queries/queries.module.ts
+++ backend/ms-queries/src/queries/queries.module.ts
@@ -4,12 +4,39 @@ import { QueriesController } from './queries.controller';
 import { QueriesRepository } from './queries.repository';
 import { DatabaseModule } from 'src/common/database';
 import { QueryDocument, QuerySchema } from './schemas/query.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AUTH_SERVICE, NOTIFICATIONS_SERVICE } from 'src/common/constants';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
       { name: QueryDocument.name, schema: QuerySchema }
+    ]),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('AUTH_HOST'),
+            port: configService.get('AUTH_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: NOTIFICATIONS_SERVICE,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configService.get('NOTIFICATIONS_HOST'),
+            port: configService.get('NOTIFICATIONS_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
     ]),
   ],
   controllers: [QueriesController],
